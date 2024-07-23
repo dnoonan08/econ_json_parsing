988219b3cc03ff7cb3323a6f9cc0a522499ce36e
Alex Campbell <scampbell10@crimson.ua.edu>
2024-07-23 17:00:49 +0200

    changed the frequency plots

--- TID_scripts/frequencylock_tid.py
+++ TID_scripts/frequencylock_tid.py
@@ -5,22 +5,28 @@ import numpy as np
 
 import matplotlib.colors as mcolors
 import matplotlib.scale
+from matplotlib.colors import ListedColormap
+import matplotlib.patches as mpatches
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 
-def getMinMaxLockingFreq(data, voltage):
-    minFreq = []
-    maxFreq = []
+def getLockingFreqs(data, voltage, starttime):
+    auto_locks = []
+    timestamp = []
     for i in range(len(data)):
         for j in range(len(data[i]['tests'])):
             if 'metadata' in data[i]['tests'][j]:
                 if f"test_pllautolock[{voltage}]" in data[i]['tests'][j]['nodeid']:
-                    minFreq.append(data[i]['tests'][j]['metadata']['min_freq'])
-                    maxFreq.append(data[i]['tests'][j]['metadata']['max_freq'])
-    minFreq = np.array(minFreq)
-    maxFreq = np.array(maxFreq)
-    mradDose = FNames2MRad(fnames)
-    return minFreq, maxFreq, mradDose
+                    auto_locks.append(data[i]['tests'][j]['metadata']['auto_locks'])
+                    timestamp.append(data[i]['tests'][j]['metadata']['timestamp'])
+    auto_locks = np.array(auto_locks)
+    mradDose = Timestamp2MRad(timestamp, starttime)
+    results = {
+        'auto_locks': auto_locks,
+        'mradDose': mradDose,
+        'dosePlots': np.array(list(mradDose)+[(mradDose[-1]-mradDose[-2])+mradDose[-1]])
+    }
+    return results
 
 if __name__ == '__main__':
     # argument parser
@@ -43,49 +49,39 @@ if __name__ == '__main__':
 
     plots = create_plot_path(args.path+ '/' + 'frequencylock_vs_tid_plots-%s'%args.chip)
 
-    MinMaxLockingFreq = {
-    volt: {
-        'minFreq': getMinMaxLockingFreq(data, volt)[0],
-        'maxFreq': getMinMaxLockingFreq(data, volt)[1],
-        'mradDose': getMinMaxLockingFreq(data, volt)[2],
-    } for volt in voltages
-}
+    autoLocks = {
+    volt: getLockingFreqs(data, volt, starttime) for volt in voltages
+    }
 
     titles = ['08', '11', '14', '20', '26', '29', '32']
     for i, (volt) in enumerate(voltages):
-        plt.plot(MinMaxLockingFreq[volt]['mradDose'], MinMaxLockingFreq[volt]['maxFreq'])
-        plt.title(f"Maximum Locking Frequency at {volt}V")
-        plt.xlabel("TID (MRad)")
+        b,a =np.meshgrid(np.arange(35, 50+(1/8), (1/8)),np.array(autoLocks[volt]['mradDose']))
+        d = autoLocks[volt]['auto_locks'].flatten()
+        binary_cmap = ListedColormap(['white', '#08306b'])
+        plt.hist2d(a.flatten(),b.flatten(),weights=d,bins=(np.array(autoLocks[volt]['dosePlots']),np.arange(35, 51+(1/8), (1/8))),cmap=binary_cmap)
+        plt.title(f"{volt}V")
         plt.ylabel("Frequency (MHz)")
-        plt.ylim(43,47)
-        plt.savefig(f'{plots}/max_locking_freq_1p{titles[i]}V.png', dpi=300, facecolor="w")
+        plt.xlabel("TID (MRad)")
+        handles, labels = plt.gca().get_legend_handles_labels()
+        patch = mpatches.Patch(color='#08306b', label='PLL Locked')
+        handles.append(patch)
+        plt.legend(handles=handles, loc='upper right')
+        plt.savefig(f'{plots}/lockingFreq_1p{titles[i]}V_ECONT.png', dpi=300, facecolor="w")
         plt.clf()
-    
+    fig,axs=plt.subplots(figsize=(70,12),ncols=7,nrows=1, layout="constrained")
     for i, (volt) in enumerate(voltages):
-        plt.plot(MinMaxLockingFreq[volt]['mradDose'], MinMaxLockingFreq[volt]['minFreq'])
-        plt.title(f"Minimum Locking Frequency at {volt}V")
-        plt.xlabel("TID (MRad)")
-        plt.ylabel("Frequency (MHz)")
-        plt.ylim(34,40)
-        plt.savefig(f'{plots}/min_locking_freq_1p{titles[i]}V.png', dpi=300, facecolor="w")
-        plt.clf()
-
-    fig,axs=plt.subplots(figsize=(55,12),ncols=7,nrows=2, layout="constrained")
-    for j, (volt) in enumerate(voltages):
-        for i in range(2):
-            if i == 0:
-                axs[i,j].plot(MinMaxLockingFreq[volt]['mradDose'], MinMaxLockingFreq[volt]['maxFreq'])
-                axs[i,j].set_title(f"Maximum Locking Frequency at {volt}V")
-                axs[i,j].set_xlabel("TID (MRad)")
-                axs[i,j].set_ylabel("Frequency (MHz)")
-                axs[i,j].set_ylim(43,47)
-            if i == 1:
-                axs[i,j].plot(MinMaxLockingFreq[volt]['mradDose'], MinMaxLockingFreq[volt]['minFreq'])
-                axs[i,j].set_title(f"Minimum Locking Frequency at {volt}V")
-                axs[i,j].set_xlabel("TID (MRad)")
-                axs[i,j].set_ylabel("Frequency (MHz)")
-                axs[i,j].set_ylim(34,40)
+        b,a =np.meshgrid(np.arange(35, 50+(1/8), (1/8)),np.array(autoLocks[volt]['mradDose']))
+        d = autoLocks[volt]['auto_locks'].flatten()
+        binary_cmap = ListedColormap(['white', '#08306b'])
+        axs[i].hist2d(a.flatten(),b.flatten(),weights=d,bins=(np.array(autoLocks[volt]['dosePlots']),np.arange(35, 51+(1/8), (1/8))),cmap=binary_cmap)
+        axs[i].set_ylabel("Frequency (MHz)")
+        axs[i].set_xlabel("TID (MRad)")
+        axs[i].set_title(f"{volt}V")
+        handles, labels = plt.gca().get_legend_handles_labels()
+        patch = mpatches.Patch(color='#08306b', label='PLL Locked')
+        handles.append(patch)
+        axs[i].legend(handles=handles, loc='upper right')
     for ax in axs.flat:
-            ax.label_outer()
-
-    fig.savefig(f'{plots}/min_max_locking_freq.png', dpi=300, facecolor="w") 
+        ax.label_outer()
+        
+    fig.savefig(f'{plots}/summary_locking_freq.png', dpi=300, facecolor="w")
