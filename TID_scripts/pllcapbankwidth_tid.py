707c4a3169ff5e35ea59803882290bec864324aa
Marko Stamenkovic <marko@msmacbook.dyndns.cern.ch>
2024-07-28 14:10:59 +0200

    Updating pllcapbank script to include timestamp and reduce tid plots to only xrays on

--- TID_scripts/pllcapbankwidth_tid.py
+++ TID_scripts/pllcapbankwidth_tid.py
@@ -1,6 +1,6 @@
 # Script to plot current versus TID 
 
-from common import get_data, Timestamp2MRad, FNames2MRad, voltages, MYROOT, create_plot_path, get_fnames
+from common import *
 import numpy as np
 
 import matplotlib.colors as mcolors
@@ -16,8 +16,15 @@ def getpllCapbankWidth(data, voltage):
                 if f"test_pll_capbank_width[{voltage}]" in data[i]['tests'][j]['nodeid']:
                     PllCapbankWidth.append(data[i]['tests'][j]['metadata']['pll_capbank_width'])    
     
-    mradDose = FNames2MRad(fnames)
-    return PllCapbankWidth, mradDose
+    #mradDose = FNames2MRad(fnames)
+    PllCapbankWidth = np.array(PllCapbankWidth)
+    times = FNames2Time(fnames)
+    times = np.array([np.datetime64(x) for x in times])
+    mradDose, hasXrays = datetime_to_TID(times, ObelixDoseRate,xray_start_stop[args.chip])
+    mradDose = np.array(mradDose)
+    hasXrays = np.array(hasXrays)
+
+    return PllCapbankWidth, mradDose, times, hasXrays
 
 if __name__ == '__main__':
     # argument parser
@@ -40,16 +47,21 @@ if __name__ == '__main__':
 
     plots = create_plot_path(args.path+ '/' + 'pllcapbank_vs_tid_plots-%s'%args.chip)
 
+
+    results = {volt: getpllCapbankWidth(data, volt) for volt in voltages}
     PllCapbankWidth = {
     volt: {
-        "PllCapbankWidth": getpllCapbankWidth(data, volt)[0],
-        "mradDose": getpllCapbankWidth(data, volt)[1],
+        "PllCapbankWidth": results[volt][0],
+        "mradDose": results[volt][1],
+        "times" : results[volt][2],
+        "hasXrays" : results[volt][3],
     } for volt in voltages
     }
 
     titles = ['08', '11', '14', '20', '26', '29', '32']
     for i, (volt) in enumerate(voltages):
-        plt.scatter(PllCapbankWidth[volt]["mradDose"], PllCapbankWidth[volt]["PllCapbankWidth"])
+
+        plt.scatter(PllCapbankWidth[volt]["mradDose"][PllCapbankWidth[volt]["hasXrays"]==True], PllCapbankWidth[volt]["PllCapbankWidth"][PllCapbankWidth[volt]["hasXrays"]==True])
         plt.title(f"{volt}V")
         plt.ylabel('PllCapbankWidth')
         plt.xlabel("TID (MRad)")
@@ -57,9 +69,39 @@ if __name__ == '__main__':
         plt.savefig(f'{plots}/pll_capbank_width_results_volt_1p{titles[i]}V.png', dpi=300, facecolor="w")
         plt.clf()
 
+    for i, (volt) in enumerate(voltages):
+        fig, ax = plt.subplots()
+        plt.scatter(PllCapbankWidth[volt]["times"], PllCapbankWidth[volt]["PllCapbankWidth"])
+        plt.title(f"{volt}V")
+        ax.set_xticklabels(ax.get_xticklabels(), rotation = 60)
+        plt.ylabel('PllCapbankWidth')
+        #plt.xlabel("TID (MRad)")
+        plt.ylim(0,20)
+        plt.savefig(f'{plots}/pll_capbank_width_results_volt_1p{titles[i]}V_time.png', dpi=300, facecolor="w")
+        plt.clf()
+
+
+    fig1,axs1=plt.subplots(figsize=(70,12),ncols=7,nrows=1, layout="constrained")
+    for i, (volt) in enumerate(voltages):
+        axs1[i].scatter(PllCapbankWidth[volt]["times"], PllCapbankWidth[volt]["PllCapbankWidth"])
+        axs1[i].set_xticklabels(axs1[i].get_xticklabels(), rotation = 60)
+        axs1[i].set_title(f"{volt}")
+        axs1[i].set_ylabel('PllCapbankWidth')
+        #axs[i].set_xlabel('TID (MRad)')
+        # axs[i].set_xlim(0,660)
+        axs1[i].set_ylim(0,20)
+        axs1[i].grid(which='minor', alpha=0.2, color='b', linestyle='--', linewidth=.3)
+        axs1[i].grid(which='major', alpha=0.2, color='b', linestyle='--', linewidth=.6)
+    #for ax in axs1.flat:
+    #    ax.label_outer()   
+    fig1.savefig(f'{plots}/pll_capbank_width_results_time.png', dpi=300, facecolor="w") 
+
+
+
     fig,axs=plt.subplots(figsize=(70,12),ncols=7,nrows=1, layout="constrained")
     for i, (volt) in enumerate(voltages):
-        axs[i].scatter(PllCapbankWidth[volt]["mradDose"], PllCapbankWidth[volt]["PllCapbankWidth"])
+        print(PllCapbankWidth[volt]["hasXrays"]==1)
+        axs[i].scatter(PllCapbankWidth[volt]["mradDose"][PllCapbankWidth[volt]["hasXrays"]==1], PllCapbankWidth[volt]["PllCapbankWidth"][PllCapbankWidth[volt]["hasXrays"]==1])
         axs[i].set_title(f"{volt}")
         axs[i].set_ylabel('PllCapbankWidth')
         axs[i].set_xlabel('TID (MRad)')
@@ -70,3 +112,5 @@ if __name__ == '__main__':
     for ax in axs.flat:
         ax.label_outer()   
     fig.savefig(f'{plots}/pll_capbank_width_results.png', dpi=300, facecolor="w") 
+
+    
