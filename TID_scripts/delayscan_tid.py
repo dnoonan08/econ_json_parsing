b5fd7f69c7fc44bbee6ee423a4edcc994dc0e660
Marko Stamenkovic <marko@msmacbook.dyndns.cern.ch>
2024-07-28 17:39:44 +0200

    conflict solving

--- TID_scripts/delayscan_tid.py
+++ TID_scripts/delayscan_tid.py
@@ -1,6 +1,6 @@
 # Script to plot current versus TID 
 
-from common import get_data, Timestamp2MRad, FNames2MRad, voltages, MYROOT, create_plot_path, get_fnames
+from common import *
 import numpy as np
 
 import matplotlib.colors as mcolors
@@ -21,9 +21,15 @@ def getDelayScanData(data, voltage):
     bitCounts = np.array(bitCounts)
     errRate = errCounts/bitCounts
 
-    mradDose = FNames2MRad(fnames)
+    #mradDose = FNames2MRad(fnames)
+    times = FNames2Time(fnames)
+    mradDose, hasXrays = datetime_to_TID(times, ObelixDoseRate, xray_start_stop[args.chip])
+    mradDose = np.array(mradDose)
+    hasXrays = np.array(hasXrays)
     dosePlots = np.array(list(mradDose)+[(mradDose[-1]-mradDose[-2])+mradDose[-1]])
-    return errRate, dosePlots, mradDose
+    timesPlots = np.array(list(times)+[(times[-1]-times[-2])+times[-1]])
+    #hasXraysPlots = np.array(list(hasXrays)+[(hasXrays[-1]-hasXrays[-2])+hasXrays[-1]])
+    return errRate, dosePlots, mradDose, hasXrays, times, timesPlots
 
 if __name__ == '__main__':
     # argument parser
@@ -50,11 +56,16 @@ if __name__ == '__main__':
 
     plots = create_plot_path(args.path+ '/' + 'delayscan_vs_tid_plots-%s'%args.chip)
 
+    results = {volt: getDelayScanData(data, volt) for volt in voltages}
     delayScan = {
         volt: {
-            'errRate': getDelayScanData(data, volt)[0],
-            'plotDose': getDelayScanData(data, volt)[1],
-            'mradDose': getDelayScanData(data, volt)[2],
+            'errRate': results[volt][0],
+            'plotDose': results[volt][1],
+            'mradDose': results[volt][2],
+            'hasXrays': results[volt][3],
+            'times': results[volt][4],
+            'timesPlots': results[volt][5],
+            #'hasXraysPlots': results[5],
         } for volt in voltages
     }
 
@@ -78,6 +89,23 @@ if __name__ == '__main__':
             plt.clf()
             plt.close()
 
+        for i, (volt) in enumerate(voltages):
+            fig, ax = plt.subplots()
+            a, b = np.meshgrid(delayScan[volt]['times'],np.arange(63))
+            weights = delayScan[volt]['errRate'][:,j,:].T.flatten()
+            h = plt.hist2d(a.flatten(), b.flatten(), weights=weights, bins = (delayScan[volt]['timesPlots'], np.arange(64)), cmap='RdYlBu_r', alpha=weights>0)
+            cb=fig.colorbar(h[3])
+            cb.set_label(label='Transmission errors rate')
+            plt.title(f"eTx {j} at {volt}V")
+            ax.set_xticklabels(ax.get_xticklabels(), rotation = 60)
+
+            plt.ylabel('Delay select setting')
+            plt.xlabel('TID (MRad)')
+            plt.savefig(f'{plots}/delay_scan_volt_1p{titles[i]}V_eRx{j}_time.png', dpi=300, facecolor="w")
+            plt.clf()
+            plt.close()
+
+
     for j in range(nbins):
 
         fig,axs=plt.subplots(figsize=(55,12),ncols=7,nrows=1, layout="constrained")
@@ -92,6 +120,22 @@ if __name__ == '__main__':
             axs[i].set_title(f"eTx {j} at {volt}V")
             axs[i].set_ylabel('Delay select setting')
             axs[i].set_xlabel('TID (MRad)')
+        for ax in axs.flat:
+            ax.label_outer()
+
+        fig,axs=plt.subplots(figsize=(55,12),ncols=7,nrows=1, layout="constrained")
+        
+        for i, (volt) in enumerate(voltages):
+            a, b = np.meshgrid(delayScan[volt]['times'],np.arange(63))
+            weights = delayScan[volt]['errRate'][:,j,:].T.flatten()
+            norm = mcolors.TwoSlopeNorm(vmin=0, vmax = 255, vcenter=.9)
+            h = axs[i].hist2d(a.flatten(), b.flatten(), weights=weights, bins = (delayScan[volt]['timesPlots'], np.arange(64)), cmap='RdYlBu_r', alpha=weights>0)
+            cb=fig.colorbar(h[3], ax = axs[i])
+            cb.set_label(label='Transmission errors rate')
+            axs[i].set_xticklabels(ax.get_xticklabels(), rotation = 60)
+            axs[i].set_title(f"eTx {j} at {volt}V")
+            axs[i].set_ylabel('Delay select setting')
+            #axs[i].set_xlabel('TID (MRad)')
         for ax in axs.flat:
             ax.label_outer()
 
